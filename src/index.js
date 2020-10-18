c6edda19f679dacfd37f1bef8c05a8f40927a1c8
Mariia415 <mariia.galitsyna@gmail.com>
2020-10-19 00:09:57 +0200

    feat: new function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
-}
+    let string = n.toString();
+    let newNumber = '';
+    for (let i = 0; i < string.length; i++){
+        newNumber = `${string[i]}${newNumber}`;
+    };
+    return Number(newNumber);
+};
